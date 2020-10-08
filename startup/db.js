0bf48864269947a53af90de68c7e6df3433e71e6
ransolo <ransolo@me.com>
2020-10-08 13:51:42 -0500

    add-catch

--- startup/db.js
+++ startup/db.js
@@ -4,8 +4,10 @@ const config = require("config");
 
 module.exports = function () {
 	const db = config.get("db");
-	console.log("dbdbdbdbdbdbdbdbd", db);
 	mongoose
-		.connect(db, { useNewUrlParser: true })
-		.then(() => winston.info(`Connected to ${db}...`));
+		.connect(db)
+		.then(() => winston.info(`Connected to ${db}...`))
+		.catch((err) => {
+			console.log("Not Connected to Database ERROR! ", err);
+		});
 };
