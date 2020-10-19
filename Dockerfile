e3233a78cbb6fcc76086f56061a9c672c6dbb800
ransolo <ransolo@me.com>
2020-10-19 10:45:29 -0500

    update-config-dev-works

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,13 @@
 FROM node:12
 WORKDIR /app
 ADD . /app
+RUN curl -fsSLO https://get.docker.com/builds/Linux/x86_64/docker-17.04.0-ce.tgz \
+	&& tar xzvf docker-17.04.0-ce.tgz \
+	&& mv docker/docker /usr/local/bin \
+	&& rm -r docker docker-17.04.0-ce.tgz
 RUN npm install
 #RUN npm install -g jenkins
 RUN npm install -g winston
-EXPOSE 3000
+
+EXPOSE 3900
 CMD npm start
--- routes/genres.js
+++ routes/genres.js
@@ -7,56 +7,60 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  const genres = await Genre.find()
-    .select("-__v")
-    .sort("name");
-  res.send(genres);
+	const genres = await Genre.find().select("-__v").sort("name");
+	res.send(genres);
 });
 
 router.post("/", auth, async (req, res) => {
-  const { error } = validate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+	const { error } = validate(req.body);
+	if (error) return res.status(400).send(error.details[0].message);
 
-  let genre = new Genre({ name: req.body.name });
-  genre = await genre.save();
+	let genre = new Genre({ name: req.body.name });
+	genre = await genre.save();
 
-  res.send(genre);
+	res.send(genre);
 });
 
 router.put("/:id", [auth, validateObjectId], async (req, res) => {
-  const { error } = validate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+	const { error } = validate(req.body);
+	if (error) return res.status(400).send(error.details[0].message);
 
-  const genre = await Genre.findByIdAndUpdate(
-    req.params.id,
-    { name: req.body.name },
-    {
-      new: true
-    }
-  );
+	const genre = await Genre.findByIdAndUpdate(
+		req.params.id,
+		{ name: req.body.name },
+		{
+			new: true,
+		},
+	);
 
-  if (!genre)
-    return res.status(404).send("The genre with the given ID was not found.");
+	if (!genre)
+		return res
+			.status(404)
+			.send("The genre with the given ID was not found.");
 
-  res.send(genre);
+	res.send(genre);
 });
 
 router.delete("/:id", [auth, admin, validateObjectId], async (req, res) => {
-  const genre = await Genre.findByIdAndRemove(req.params.id);
+	const genre = await Genre.findByIdAndRemove(req.params.id);
 
-  if (!genre)
-    return res.status(404).send("The genre with the given ID was not found.");
+	if (!genre)
+		return res
+			.status(404)
+			.send("The genre with the given ID was not found.");
 
-  res.send(genre);
+	res.send(genre);
 });
 
 router.get("/:id", validateObjectId, async (req, res) => {
-  const genre = await Genre.findById(req.params.id).select("-__v");
+	const genre = await Genre.findById(req.params.id).select("-__v");
 
-  if (!genre)
-    return res.status(404).send("The genre with the given ID was not found.");
+	if (!genre)
+		return res
+			.status(404)
+			.send("The genre with the given ID was not found.");
 
-  res.send(genre);
+	res.send(genre);
 });
 
 module.exports = router;
