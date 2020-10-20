a35baf40e4ade08c5172ea2c1e77b890831b2e24
ransolo <ransolo@me.com>
2020-10-20 14:38:50 -0500

    add-dep

--- startup/db.js
+++ startup/db.js
@@ -5,7 +5,7 @@ const config = require("config");
 module.exports = function () {
 	const db = config.get("db");
 	mongoose
-		.connect(db)
+		.connect(db, { useNewUrlParser: true }, { useUnifiedTopology: true })
 		.then(() => winston.info(`Connected to ${db}...`))
 		.catch((e) =>
 			winston.info(`Not connected to ${db}:------------- ${e}`),
