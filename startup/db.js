81ee7c8c59dcf4f4d7fc1e8b2c2c7337c50e752f
ransolo <ransolo@me.com>
2020-10-20 15:24:14 -0500

    add-wait

--- startup/db.js
+++ startup/db.js
@@ -7,6 +7,11 @@ module.exports = function () {
 	mongoose
 		.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
 		.then(() => winston.info(`Connected to ${db}...`))
+		.then(
+			new Promise((resolve) => {
+				setTimeout(resolve, 5000);
+			}),
+		) // lets wait 5 second before connection to MongoDB)
 		.catch((e) =>
 			winston.info(`Not connected to ${db}:------------- ${e}`),
 		);
