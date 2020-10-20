5f6cf04c4c68900177a84f4bc26343a9a6361428
ransolo <ransolo@me.com>
2020-10-20 12:02:04 -0500

    troubleshoot

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ require("./startup/config")();
 require("./startup/validation")();
 
 const port = process.env.PORT || config.get("port");
-const server = app.listen(port, () => {
-	winston.info(`Listening on port ${port}...`);
-});
+const server = app.listen(port, () =>
+	winston.info(`Listening on port ${port}...`),
+);
 
 module.exports = server;
--- startup/db.js
+++ startup/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const config = require("config");
 
 module.exports = function () {
-	const db = "mongodb://db:27017/myapp";
+	const db = config.get("db");
 	mongoose
 		.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
 		.then(() => winston.info(`Connected to ${db}...`))
