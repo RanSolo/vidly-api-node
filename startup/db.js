68dfe78edec54377e466755529eaf86b3cd926c5
ransolo <ransolo@me.com>
2020-10-20 11:56:37 -0500

    hardcode-db-url

--- startup/db.js
+++ startup/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const config = require("config");
 
 module.exports = function () {
-	const db = config.get("db");
+	const db = "mongodb://db:27017/myapp";
 	mongoose
 		.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
 		.then(() => winston.info(`Connected to ${db}...`))
