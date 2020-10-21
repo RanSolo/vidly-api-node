19174e6ec10803bfb09d039e8466b551f4787a2c
ransolo <ransolo@me.com>
2020-10-21 15:06:59 -0500

    trythigs

--- startup/db.js
+++ startup/db.js
@@ -3,21 +3,20 @@ const mongoose = require("mongoose");
 const config = require("config");
 
 module.exports = function () {
-	const db = config.get("db");
-	const options = {
-		autoIndex: false, // Don't build indexes
-		reconnectTries: 30, // Retry up to 30 times
-		reconnectInterval: 500, // Reconnect every 500ms
-		poolSize: 10, // Maintain up to 10 socket connections
-		// If not connected, return errors immediately rather than waiting for reconnect
-		bufferMaxEntries: 0,
+	mongoose.connect(config.get("db"), {
 		useNewUrlParser: true,
-	};
+		useUnifiedTopology: true,
+	});
 
-	mongoose
-		.connect(db, options)
-		.then(() => winston.info(`Connected to ${db}...`))
-		.catch((e) =>
-			winston.info(`Not connected to ${db}:------------- ${e}`),
-		);
+	var db = mongoose.connection;
+	db.on("error", (e) => {
+		winston.info(`Not connected to ${db}:------------- ${e}`);
+		process.exit(1);
+	});
+	db.once("open", function () {
+		() => {
+			winston.info(`Connected to ${db}...`);
+			callback();
+		};
+	});
 };
