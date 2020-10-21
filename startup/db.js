138578aa5af85899f32e3e1d5e5e3e8e25311dbd
ransolo <ransolo@me.com>
2020-10-21 17:46:09 -0500

    switch-back

--- startup/db.js
+++ startup/db.js
@@ -5,6 +5,10 @@ const config = require("config");
 module.exports = function () {
 	const db = config.get("db");
 	mongoose
-		.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+		.connect(db, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useFindAndModify: true,
+		})
 		.then(() => winston.info(`Connected to ${db}...`));
 };
