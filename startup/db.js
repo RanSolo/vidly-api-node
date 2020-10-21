4ae5fc8ea59bc8f249be90d32ea6e7489de9ad09
ransolo <ransolo@me.com>
2020-10-21 14:12:35 -0500

    try

--- startup/db.js
+++ startup/db.js
@@ -4,8 +4,19 @@ const config = require("config");
 
 module.exports = function () {
 	const db = config.get("db");
+	const options = {
+		autoIndex: false, // Don't build indexes
+		reconnectTries: 30, // Retry up to 30 times
+		reconnectInterval: 500, // Reconnect every 500ms
+		poolSize: 10, // Maintain up to 10 socket connections
+		// If not connected, return errors immediately rather than waiting for reconnect
+		bufferMaxEntries: 0,
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	};
+
 	mongoose
-		.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+		.connect(db, options)
 		.then(() => winston.info(`Connected to ${db}...`))
 		.catch((e) =>
 			winston.info(`Not connected to ${db}:------------- ${e}`),
